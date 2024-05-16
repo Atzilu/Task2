0b8b2c3cd3f9404e72660a737b6d01f710766341
Atzilu <forolymp30032009@gmail.com>
2024-05-16 20:28:00 +0300

    Add files via upload

---
+++ task2mod.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> increaseVector(vector<int> vec, int n)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        vec[i] += n;
+    }
+    return vec;
+}
+
+int main()
+{
+    int C;
+    cout << "Введите кол-во элементов в векторе:" << endl;
+    cin >> C;
+    vector<int> vec = { C };
+    int n;
+    cout << "Введите n:" << endl;
+    cin >> n;
+    cout << "Введите вектор:" << endl;
+    for (auto& it : vec)
+    {
+        cin >> it;
+    }
+    vector<int> result = increaseVector(vec, n);
+    for (int i = 0; i >= result.size(); i++)
+    {
+        cout << result[i] << endl;
+    }
+    return 0;
+}
---
+++ task2test.cpp
@@ -0,0 +1,10 @@
+﻿#include <gtest/gtest.h>
+#include ".../task2/"
+
+TEST(IncreaseVectorTest, BasicTest)
+{
+    vector<int> input = { 1, 2, 3 };
+    vector<int> expected = { 4, 5, 6 };
+    vector<int> result = increaseVector(input, 3);
+    ASSERT_EQ(expected, result);
+}
